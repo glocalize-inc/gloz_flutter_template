471b813fb06864af4d38456cffd57e95786b571c
Terry <yungik@glocalizeinc.com>
2022-07-25 13:41:14 +0900

    Add init firebase

---
+++ lib/src/firebase/init_firebase.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+
+import 'firebase_options.dart';
+
+late FirebaseCrashlytics crashlytics;
+
+Future<void> initializeFirebase({
+  // bool useRealtimeDatabase = true,
+  bool useCrashlytics = true,
+}) async {
+  if (!useCrashlytics) {
+    if (kDebugMode) {
+      print('파이어베이스를 사용하지 않습니다.');
+    }
+  }
+
+  try {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  } on FirebaseException catch (e) {
+    switch (e.code) {
+      case 'duplicate-app':
+        Firebase.app();
+        break;
+      default:
+        rethrow;
+    }
+  }
+
+  // if (useRealtimeDatabase) {
+  //   rtdb = FirebaseDatabase.instance;
+  // }
+
+  if (useCrashlytics) {
+    crashlytics = FirebaseCrashlytics.instance;
+  }
+}
