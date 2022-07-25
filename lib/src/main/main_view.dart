e65515b5c053f14813c76de2d7eac9d11eade3af
Terry <yungik@glocalizeinc.com>
2022-07-25 13:42:15 +0900

    Add main view

---
+++ lib/src/main/main_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainView extends StatelessWidget {
+  static const String routeName = '/';
+
+  const MainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //TODO
+    return const Scaffold(body: Text('Hi world!'));
+  }
+}
